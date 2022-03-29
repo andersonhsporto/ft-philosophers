f3a626d385c16ea99f9834261a580b459b34c874
Anderson Porto <andersonhigo2@gmail.com>
2022-03-29 03:43:05 -0300

    build: single philo

--- philo/3_2_threads_routine.c
+++ philo/3_2_threads_routine.c
@@ -6,7 +6,7 @@
 /*   By: anhigo-s <anhigo-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 00:08:16 by anhigo-s          #+#    #+#             */
-/*   Updated: 2022/03/29 03:30:58 by anhigo-s         ###   ########.fr       */
+/*   Updated: 2022/03/29 03:42:24 by anhigo-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,20 @@ void	*main_routine(void *ptr)
 
 	list = (t_thinker *)ptr;
 	list->time_start = timenow();
-	if (list->index % 2)
+	list->last_meal = list->time_start;
+	if (list->index % 2 == 0)
 		new_usleep(list->data->args.time_eat);
 	while (list->data->philo_alive == true)
 	{
+		if (list->data->args.nbr_philo == 1)
+		{
+			pthread_mutex_lock(&list->fork);
+			pthread_mutex_lock(&list->data->printer_mutex);
+			printf("%zu\t%s %d\n", \
+				(timenow() - list->time_start), FORK, list->index);
+			pthread_mutex_unlock(&list->data->printer_mutex);
+			new_usleep(list->data->args.time_die);
+		}
 		fork_lock(list);
 		lunchtime(list);
 		fork_unlock(list);
